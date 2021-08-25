f640e2ca6f2896fe3076e197dbfa0b440e5e2017
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-08-25 09:27:28 -0400

    added getLocation function and location state to App.js

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Nasa from './components/NASA/Nasa'
 import OpenWeather from './components/OpenWeather/OpenWeather'
@@ -6,12 +6,22 @@ import TicketMaster from './components/TicketMaster/TicketMaster'
 
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [coordinates, setCoordinates] = useState({})
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition( // THIS JS METHOD ASKS CLIENT FOR PERMISSION TO USE POSITION
+      position => {                           // TAKES A CALLBACK
+      console.log(position)                   // LOG POSITION SO WE CAN SEE WHAT WE ARE WORKING WITH
+      setCoordinates({ lat: position.coords.latitude, lon: position.coords.longitude }) // SET STATE OBJECT 'coordinates'... { coordinates.lat } and { coordinates.lon } 
+    })
+  }
+
+  useEffect(getLocation, []) // RUN getLocation() WHEN App.js mounts
 
   return (
     <div className="App">
       <h1>Hello, World</h1>
-      <Nasa />
+      <Nasa coordinates={coordinates}/> {/*PASSING COORDINATES STATE TO NASA CHILD*/}
       <OpenWeather />
       <TicketMaster />
     </div>
--- my-vue-app/src/components/NASA/Nasa.jsx
+++ my-vue-app/src/components/NASA/Nasa.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react"
 
-const Nasa = (props) => {
+const Nasa = props => {
+  const { lat, lon } = props.coordinates
+
   const apiKey = "qwOijB0BlWxLDopTggwefGV4GwqvhBJ1nlbFRw5M"
 
   const [url, setUrl] = useState('')
-  const [coordinates, setCoordinates] = useState({})
   const [nasaPhoto, setNasaPhoto] = useState(null)
 
   const fetchFromNasa = async () => {
     // console.log('Checking for coordinates')
-    if (!coordinates.lat || !coordinates.lon) return // I THINK HAD TO ADD THIS LINE B/C OF STRING INTERPOLATION IN handleCoordinates()
+    if (!lat || !lon) return // I THINK HAD TO ADD THIS LINE I THINK B/C OF STRING INTERPOLATION IN handleCoordinates()
     // console.log('Fetching from Nasa')
 
     const res = await fetch(url + apiKey)
@@ -18,22 +19,14 @@ const Nasa = (props) => {
     setNasaPhoto(URL.createObjectURL(blob))
   }
 
-  const getLocation = () => {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      setCoordinates({ lat: pos.coords.latitude, lon: pos.coords.longitude })
-    })
-    // console.log('Got location, set coordinates')
-  }
-
   const handleCoordinates = () => {
     setUrl(
-      `https://api.nasa.gov/planetary/earth/imagery?lon=${coordinates.lon}&lat=${coordinates.lat}&dim=.03&api_key=`
+      `https://api.nasa.gov/planetary/earth/imagery?lon=${lon}&lat=${lat}&dim=.03&api_key=`
     )
     // console.log('Set the URL')
   }
 
-  useEffect(getLocation, [])
-  useEffect(handleCoordinates, [coordinates])
+  useEffect(handleCoordinates, [props.coordinates])
   useEffect(fetchFromNasa, [url])
 
   return (
@@ -51,10 +44,3 @@ const Nasa = (props) => {
 }
 
 export default Nasa
-
-/*
-Things i have done:
- - added fetch to Nasa component
- - added useState, useEffect 
-
-*/
