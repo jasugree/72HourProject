3d06fc38f1ad0f71f7847dd7bf8c3a00b30eafba
Jake Greene <jsgreene208@gmail.com.com>
2021-08-26 12:04:39 -0400

    Creating Jake's Branch

--- my-vue-app/src/components/OpenWeather/OpenWeather.jsx
+++ my-vue-app/src/components/OpenWeather/OpenWeather.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
+const key = '4e3b2fbc699bc6dfe7707088a630fb92';
+const city =  'Westfield'
+const state = 'Indiana'
+
+let measurement = 'imperial'
+
+let symbol = "F";
 
 const OpenWeather = (props) => {
+
+
+
+const [temperature, settemperature] = useState("");
+const [icon, seticon] = useState();
+
+
+
+
+
+  const fetchResults = async() => {
+    let url = `${baseURL}?q=${city},${state}&appid=${key}&units=${measurement}`;  
+    const response = await fetch(url)
+    const data = await response.json()
+
+    console.log(data);
+    settemperature(data.main.temp);
+    seticon(data.weather[0].icon)
+  }
+
+  useEffect(() => {
+    fetchResults();  
+  },[]);
+  
+
+
+  const unitChange = () =>{
+    console.log('This button got clicked')
+    if(measurement === 'metric'){
+      measurement = 'imperial'
+      symbol = "F";
+    } else if(measurement === 'imperial'){
+      measurement = 'metric'
+      symbol = "C";
+    }else{
+      console.log('nothing happened')
+    }
+    fetchResults();  
+  }
+
+  
   return ( 
-    <div>
-      OpenWeather
+    <div className="weatherApp">
+      <h2>OpenWeather API</h2>
+      <img className="weatherIcon" src={`https://openweathermap.org/img/w/${icon}.png`} alt="icon" />
+            <br />
+      <p> The temperature is: <span className="temperature">{temperature} °{symbol}</span></p>
+      <button className="weatherButton" onClick={unitChange}>Change Unit</button>
+
     </div>
    );
 }
