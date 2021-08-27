3e39c1dde49c028a3bf9247e1811288bbd315ce1
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-08-26 21:15:09 -0400

    wrapping up thursday night nasa

--- my-vue-app/src/components/OpenWeather/OpenWeather.jsx
+++ my-vue-app/src/components/OpenWeather/OpenWeather.jsx
@@ -3,11 +3,6 @@ import React, { useState, useEffect } from 'react';
 
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
 const key = '4e3b2fbc699bc6dfe7707088a630fb92';
-<<<<<<< HEAD
-const city =  'Westfield'
-const state = 'US-IN'
-=======
->>>>>>> e328b675d7016287fee47f807f6f0015cd04f056
 
 let measurement = 'imperial'
 
@@ -16,15 +11,9 @@ let symbol = "F";
 const OpenWeather = (props) => {
 const {lat, lon} = props.coordinates 
 
-<<<<<<< HEAD
-const [temperature, settemperature] = useState("");
-const [icon, seticon] = useState();
-
-=======
 const [url, seturl] = useState("");
 const [temperature, settemperature] = useState("");
 const [icon, seticon] = useState("50d");
->>>>>>> e328b675d7016287fee47f807f6f0015cd04f056
 
   const fetchResults = async() => {
     if (!lat || !lon) return
