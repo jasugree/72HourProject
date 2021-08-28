c4020dcc3cf02ea2850266edd582cdfaba95afc2
Marcus Elliott <webdevhero2010@gmail.com>
2021-08-28 10:08:15 -0400

    ryan and team fixed button

--- my-vue-app/src/components/TicketMaster/TicketMaster.jsx
+++ my-vue-app/src/components/TicketMaster/TicketMaster.jsx
@@ -42,6 +42,11 @@ const TicketMaster = (props) => {
           <td>{event.type}</td>
           <td>{event.dates.start.localDate}</td>
           <td>{event.distance} mi.</td>
+          <td>
+            <a href={event.url} target="_blank" rel="noopener noreferrer">
+              <Button color="primary">Purchase Tickets</Button>
+            </a>
+          </td>
         </tr>
       );
     });
@@ -59,6 +64,7 @@ const TicketMaster = (props) => {
               <th>type:</th>
               <th>date:</th>
               <th>distance:</th>
+              <th>link:</th>
             </tr>
           </thead>
           <tbody>{eventList ? tmEventMapper() : null}</tbody>
